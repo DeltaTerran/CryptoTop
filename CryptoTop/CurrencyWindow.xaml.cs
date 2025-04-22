9f22e709ba44eae34b318796624d24bb048fe4b6
DeltaTerran <96633825+DeltaTerran@users.noreply.github.com>
2025-04-22 17:35:26 +0300

    Created Form for Main Window

---
+++ CryptoTop/CurrencyWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CryptoTop
+{
+   
+    public partial class CurrencyWindow : Window
+    {
+        List<Currencies> _currencyList = new List<Currencies>();
+        //List<Exchange> _exchangeList = new List<Exchange>();
+        public CurrencyWindow()
+        {
+            InitializeComponent();
+            Currencies bitCoint = new Currencies("bitcoin", 1, "BTC", 19752815.0000000000000000, 21000000.0000000000000000, 1134508584478.0989721079862315, 7243846863.3409126543165751, 57435.2862859343831301, -0.0461491427646531, 57868.1484672081301126, "https://blockchain.info/werweqrerwerw");
+            Currencies ethereum = new Currencies("ethereum", 2, "ETH", 120703159.4902972300000000, 0, 193777781868.9090343683484662, 2299983274.2853347082736113, 1605.4077017303423284, 1.0200787391906480, 1595.4561307255059976, "https://etherscan.io/");
+            Currencies tether = new Currencies("tether", 3, "USDT", 144640112847.9336500000000000, 0, 144728930413.6819067662333179, 12880285449.0990439390019568, 1.0006140590186184, 0.0140617581020110, 1.0001427460269649, "https://www.omniexplorer.info/asset/31");
+            _currencyList.Add(bitCoint);
+            _currencyList.Add(ethereum);
+            _currencyList.Add(tether);
+            _topCurrencies.ItemsSource = _currencyList;
+        }
+
+        private void _topCurrencies_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //var currencies = _topCurrencies.ItemsSource as IEnumerable<Currencies>;
+            //var curr = currencies.ElementAt(_topCurrencies.SelectedIndex);
+            //PriceInfo.Content = curr.Price;
+            //VolumeInfo.Content = curr.Volume;
+            //PriceChangeInfo.Content = curr.ChangePercent;
+            //MarketInfo.Content = curr.Link;
+            //DebugLabelMid.Content = curr.Id.ToString();
+            //DebugLabelLeftMid.Content = curr.Price;
+            //DebugLabelTopLeft.Content = curr.Volume;
+            //DebugLabelTopMid.Content = curr.ChangePercent;
+            //DebugLabelBottomLeft.Content = curr.Link;
+
+
+        }
+    }
+    public class Currencies
+    {
+        public Currencies(string id, int rank, string symbol, double supply, double maxSupply, double marketCup, double volume, double price, double changePercent, double vWAP, string link)
+        {
+            Id = id;
+            Rank = rank;
+            Symbol = symbol;
+            Supply = supply;
+            MaxSupply = maxSupply;
+            MarketCup = marketCup;
+            Volume = volume;
+            Price = price;
+            ChangePercent = changePercent;
+            VWAP = vWAP;
+            Link = link;
+        }
+
+        public string Id { get; set; } = "";
+        public int Rank { get; set; }
+        public string Symbol { get; set; } = "";
+        public double Supply { get; set; }
+        public double MaxSupply { get; set; }
+        public double MarketCup { get; set; }
+        public double Volume { get; set; }
+        public double Price { get; set; }
+        public double ChangePercent { get; set; }
+        public double VWAP { get; set; }
+        public string Link { get; set; }
+
+
+
+    }
+    //private void _topCurrencies_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    //{
+    //    DebugLabel.Content = _topCurrencies.SelectedValue;
+    //}
+
+    //    public class Exchange
+    //    {
+    //        public string Name { get; set; }
+    //        public double Price { get; set; }
+    //        public int Volume { get; set; }
+
+    //        public double Change { get; set; }
+
+    //        public string Markets { get; set; }
+
+
+
+    //        public Exchange(string name, double price, int volume, double change, string markets)
+    //        {
+    //            Name = name;
+    //            Price = price;
+    //            Volume = volume;
+    //            Change = change;
+    //            Markets = markets;
+    //        }
+    //    }
+}
