1a7162d4fb59e34fd30a7801f12c15e8e9842fea
DeltaTerran <96633825+DeltaTerran@users.noreply.github.com>
2025-04-23 15:16:24 +0300

    десериализация JSON

---
+++ CryptoTop/Classes/JsonHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CryptoTop.Classes
+{
+    public static class JsonHandler
+    {
+        static string _filePath = "C:\\Users\\spaka\\source\\repos\\CryptoTop\\CryptoTop\\Json\\Assets.JSON";
+        static string _jsonFromFile = File.ReadAllText(_filePath);
+
+        public static List<Currencies> CreateList()
+        {
+            CurrencyResponse response = JsonSerializer.Deserialize<CurrencyResponse>(_jsonFromFile);
+            List<Currencies> temp = response.data;
+            return temp;
+        }
+    }
+    public class CurrencyResponse
+    {
+        public List<Currencies> data { get; set; }
+        public long timestamp { get; set; }
+    }
+
+    public class Currencies
+    {
+        public string id { get; set; }
+        public string rank { get; set; }
+        public string symbol { get; set; }
+        public string name { get; set; }
+        public string supply { get; set; }
+        public string maxSupply { get; set; }
+        public string marketCapUsd { get; set; }
+        public string volumeUsd24Hr { get; set; }
+        public string priceUsd { get; set; }
+        public string changePercent24Hr { get; set; }
+        public string vwap24Hr { get; set; }
+        public string explorer { get; set; }
+    }
+}
--- CryptoTop/CurrencyWindow.xaml.cs
+++ CryptoTop/CurrencyWindow.xaml.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using CryptoTop.Classes;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -18,21 +22,31 @@ namespace CryptoTop
     public partial class CurrencyWindow : Window
     {
         List<Currencies> _currencyList = new List<Currencies>();
-        //List<Exchange> _exchangeList = new List<Exchange>();
+        #region потом удалить
+        static string _filePath = "C:\\Users\\spaka\\source\\repos\\CryptoTop\\CryptoTop\\Json\\Assets.JSON";
+        static string _jsonFromFile = File.ReadAllText(_filePath);
+        #endregion
         public CurrencyWindow()
         {
             InitializeComponent();
-            Currencies bitCoint = new Currencies("bitcoin", 1, "BTC", 19752815.0000000000000000, 21000000.0000000000000000, 1134508584478.0989721079862315, 7243846863.3409126543165751, 57435.2862859343831301, -0.0461491427646531, 57868.1484672081301126, "https://blockchain.info/werweqrerwerw");
-            Currencies ethereum = new Currencies("ethereum", 2, "ETH", 120703159.4902972300000000, 0, 193777781868.9090343683484662, 2299983274.2853347082736113, 1605.4077017303423284, 1.0200787391906480, 1595.4561307255059976, "https://etherscan.io/");
-            Currencies tether = new Currencies("tether", 3, "USDT", 144640112847.9336500000000000, 0, 144728930413.6819067662333179, 12880285449.0990439390019568, 1.0006140590186184, 0.0140617581020110, 1.0001427460269649, "https://www.omniexplorer.info/asset/31");
-            _currencyList.Add(bitCoint);
-            _currencyList.Add(ethereum);
-            _currencyList.Add(tether);
-            _topCurrencies.ItemsSource = _currencyList;
+            _currencyList = JsonHandler.CreateList();
+            #region Тестовые код
+
+
+            //Currencies bitCoint = new Currencies("bitcoin", 1, "BTC", 19752815.0000000000000000, 21000000.0000000000000000, 1134508584478.0989721079862315, 7243846863.3409126543165751, 57435.2862859343831301, -0.0461491427646531, 57868.1484672081301126, "https://blockchain.info/werweqrerwerw");
+            //Currencies ethereum = new Currencies("ethereum", 2, "ETH", 120703159.4902972300000000, 0, 193777781868.9090343683484662, 2299983274.2853347082736113, 1605.4077017303423284, 1.0200787391906480, 1595.4561307255059976, "https://etherscan.io/");
+            //Currencies tether = new Currencies("tether", 3, "USDT", 144640112847.9336500000000000, 0, 144728930413.6819067662333179, 12880285449.0990439390019568, 1.0006140590186184, 0.0140617581020110, 1.0001427460269649, "https://www.omniexplorer.info/asset/31");
+            //_currencyList.Add(bitCoint);
+            //_currencyList.Add(ethereum);
+            //_currencyList.Add(tether);
+            //_currencyList = JsonHandler.CreateList();
+            #endregion
+            _topCurrencies.ItemsSource = _currencyList.Take(10);
         }
 
         private void _topCurrencies_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            #region Тестовый код
             //var currencies = _topCurrencies.ItemsSource as IEnumerable<Currencies>;
             //var curr = currencies.ElementAt(_topCurrencies.SelectedIndex);
             //PriceInfo.Content = curr.Price;
@@ -44,42 +58,91 @@ namespace CryptoTop
             //DebugLabelTopLeft.Content = curr.Volume;
             //DebugLabelTopMid.Content = curr.ChangePercent;
             //DebugLabelBottomLeft.Content = curr.Link;
-
+            #endregion
 
         }
     }
-    public class Currencies
-    {
-        public Currencies(string id, int rank, string symbol, double supply, double maxSupply, double marketCup, double volume, double price, double changePercent, double vWAP, string link)
-        {
-            Id = id;
-            Rank = rank;
-            Symbol = symbol;
-            Supply = supply;
-            MaxSupply = maxSupply;
-            MarketCup = marketCup;
-            Volume = volume;
-            Price = price;
-            ChangePercent = changePercent;
-            VWAP = vWAP;
-            Link = link;
-        }
+    #region тестовые классы
+    //public class Currencies
+    //{
+    //    public Currencies(string id, int rank, string symbol, double supply, double maxSupply, double marketCup, double volume, double price, double changePercent, double vWAP, string link)
+    //    {
+    //        Id = id;
+    //        Rank = rank;
+    //        Symbol = symbol;
+    //        Supply = supply;
+    //        MaxSupply = maxSupply;
+    //        MarketCap = marketCup;
+    //        Volume = volume;
+    //        Price = price;
+    //        ChangePercent = changePercent;
+    //        VWAP = vWAP;
+    //        Link = link;
+    //    }
+    //    /// <summary>
+    //    /// id
+    //    /// </summary>
+    //    [JsonPropertyName("id")]
+    //    public string Id { get; set; } = "";
+    //    /// <summary>
+    //    /// rank
+    //    /// </summary>
+    //    [JsonPropertyName("rank")]
+    //    public int Rank { get; set; }
+    //    /// <summary>
+    //    /// symbol
+    //    /// </summary>
+    //    [JsonPropertyName("symbol")]
+    //    public string Symbol { get; set; } = "";
+    //    /// <summary>
+    //    /// supply
+    //    /// </summary>
+    //    [JsonPropertyName("supply")]
+    //    public double Supply { get; set; }
+    //    /// <summary>
+    //    /// maxsupply
+    //    /// </summary>
+    //    [JsonPropertyName("maxsupply")]
+    //    public double MaxSupply { get; set; }
+    //    /// <summary>
+    //    /// marketCapUSD
+    //    /// </summary>
+    //    [JsonPropertyName("marketCapUSD")]
+    //    public double MarketCap { get; set; }
+    //    /// <summary>
+    //    /// volumeUsd24Hr
+    //    /// </summary>
+    //    [JsonPropertyName("volumeUsd24Hr")]
+    //    public double Volume { get; set; }
+    //    /// <summary>
+    //    /// priceUsd
+    //    /// </summary>
+    //    [JsonPropertyName("priceUsd")]
+    //    public double Price { get; set; }
+    //    /// <summary>
+    //    /// changePercent24Hr
+    //    /// </summary>
+    //    [JsonPropertyName("changePercent24Hr")]
+    //    public double ChangePercent { get; set; }
+    //    /// <summary>
+    //    /// vwap24Hr
+    //    /// </summary>
+    //    [JsonPropertyName("vwap24Hr")]
+    //    public double VWAP { get; set; }
+    //    /// <summary>
+    //    /// explorer
+    //    /// </summary>
+    //    [JsonPropertyName("explorer")]
+    //    public string Link { get; set; }
 
-        public string Id { get; set; } = "";
-        public int Rank { get; set; }
-        public string Symbol { get; set; } = "";
-        public double Supply { get; set; }
-        public double MaxSupply { get; set; }
-        public double MarketCup { get; set; }
-        public double Volume { get; set; }
-        public double Price { get; set; }
-        public double ChangePercent { get; set; }
-        public double VWAP { get; set; }
-        public string Link { get; set; }
 
 
+    //}
+    //public class CurrencyResponse
+    //{
+    //    public List<Currencies> Data { get; set; }  // Свойство "data" из JSON
+    //}
 
-    }
     //private void _topCurrencies_SelectionChanged(object sender, SelectionChangedEventArgs e)
     //{
     //    DebugLabel.Content = _topCurrencies.SelectedValue;
@@ -106,4 +169,5 @@ namespace CryptoTop
     //            Markets = markets;
     //        }
     //    }
+    #endregion
 }
